5bab90439d1353e204420a7810cf43153177207f
Ravindra Lodhi <rlodhi.c@flyakeed.com>
2024-10-21 12:37:52 +0530

    Verify the database connection before starting the server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 import connectDB from './config/db';
-import {authRoute,expenseRoute} from './routes/index';
-import globalErrorHandler from "./middlewares/errorHandlerMiddleware";
+import { authRoute, expenseRoute } from './routes/index';
+import globalErrorHandler from './middlewares/errorHandlerMiddleware';
 import { setupSwagger } from './swagger';
 
 dotenv.config();
@@ -11,21 +11,20 @@ const app = express();
 
 app.use(express.json());
 
-connectDB();
-
 setupSwagger(app);
 
 app.use('/api/auth', authRoute);
 app.use('/api', expenseRoute);
 
-app.get("/", (req, res) => {
-  res.json({ error: false, msg: "success" });
+app.get('/', (req, res) => {
+  res.json({ error: false, msg: 'success' });
 });
 
 app.use(globalErrorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
